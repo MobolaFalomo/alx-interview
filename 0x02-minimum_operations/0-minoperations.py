c17f7dc69d0ea5b192e9d3a2db061c7d331fc7f6
Mobola Falomo <mobolafalomo@gmail.com>
2023-08-13 21:27:56 +0100

    minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def minOperations(n):
+    len_H = 1
+    len_copied_H = 0
+    total_operations = 0
+
+    while len_H < n:
+        if n % len_H == 0:
+            total_operations += 2 
+            len_copied_H = len_H
+            len_H *= 2
+        else:
+            total_operations += 1
+            len_H += len_copied_H
+    return total_operations
