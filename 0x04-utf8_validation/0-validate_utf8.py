fb778bff3d9ad4a66248771391f2a1c7a62cb768
Mobola Falomo <mobolafalomo@gmail.com>
2023-08-27 21:02:04 +0100

    UTF8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-""" Method that determines if a given data set represents a valid UTF-8 encoding"""
+"""UTF-8 Validation"""
+
 
 def validUTF8(data):
-    # Initialize a variable to keep track of the number of bytes left for the current character
-    bytes_left = 0
-    
-    # Iterate through each integer in the data list
-    for num in data:
-        # If bytes_left is 0, we are starting a new character
-        if bytes_left == 0:
-            # Count the number of leading '1' bits to determine the number of bytes for this character
-            mask = 1 << 7
-            while mask & num:
-                bytes_left += 1
-                mask >>= 1
-                
-            # Handle invalid cases where the count is not within the range 1 to 4
-            if bytes_left == 1 or bytes_left > 4:
+    """
+    method that determines if a given data set
+    represents a valid UTF-8 encoding
+    """
+    nbytes = 0
+
+    b1 = 1 << 7
+    b2 = 1 << 6
+
+    for i in data:
+        b = 1 << 7
+        if nbytes == 0:
+            while b & i:
+                nbytes += 1
+                b = b >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
-            
-            # Decrement bytes_left for the first byte since it's already processed
-            bytes_left -= 1
         else:
-            # Check if the current byte starts with '10' as the two most significant bits
-            if num >> 6 != 0b10:
+            if not (i & b1 and not (i & b2)):
                 return False
-            # Decrement bytes_left for subsequent bytes
-        bytes_left -= 1
-    
-    # If there are still bytes left after processing all data, it's an incomplete sequence
-    return bytes_left == 0
+        nbytes -= 1
+    return nbytes == 0
